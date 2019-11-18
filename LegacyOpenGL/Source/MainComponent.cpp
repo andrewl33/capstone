6ea6383450c85454845b1df1a9bacd9ebe5c3b5b
Andrew Lee <and.rob.lee@gmail.com>
2019-11-17 18:01:27 -0800

    Messaging errors

---
+++ LegacyOpenGL/Source/MainComponent.cpp
@@ -0,0 +1,225 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+#include <OpenGL/gl.h>
+#include <OpenGl/glu.h>
+#include <GLUT/glut.h>
+#define PI 3.14159265f
+//==============================================================================
+MainComponent::MainComponent()
+{
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (800, 600);
+
+    // Some platforms require permissions to open input channels so request that here
+    if (RuntimePermissions::isRequired (RuntimePermissions::recordAudio)
+        && ! RuntimePermissions::isGranted (RuntimePermissions::recordAudio))
+    {
+        RuntimePermissions::request (RuntimePermissions::recordAudio,
+                                     [&] (bool granted) { if (granted)  setAudioChannels (2, 2); });
+    }
+    else
+    {
+        // Specify the number of input and output channels that we want to open
+        setAudioChannels (2, 2);
+    }
+
+	r = Random();
+	// Fill pair array
+	for (auto i = 0; i < blobCount; ++i)
+	{
+		blobState.push_back(std::make_pair(std::vector<float>{0.0, 0.0, 0.0, 0.0}, std::vector<float>{0.0, 0.0, 0.0, 0.0}));
+		curState.push_back(std::vector<float>{0.0,0.0,0.0,0.0});
+	}
+
+	createNextState();
+	copyNextToCurState();
+	translate(0);
+
+	// startTimer(1000 / updatePerSecond);
+    
+    openGLContext.setComponentPaintingEnabled(true);
+    openGLContext.setRenderer(this);
+    //    openGLContext.setContinuousRepainting(true);
+    openGLContext.attachTo(*this);
+}
+
+void MainComponent::timerCallback()
+{
+	if (transitionCounter++ > translateTimeHz + pauseTimeHz) {
+		copyNextToCurState();
+		createNextState();
+		transitionCounter = 0;
+	}
+	else if (transitionCounter > translateTimeHz) {
+		return;
+	}
+	translate((transitionCounter/(float)translateTimeHz));
+	//repaint();
+    renderOpenGL();
+}
+
+MainComponent::~MainComponent()
+{
+    // This shuts down the audio device and clears the audio source.
+    shutdownAudio();
+    openGLContextClosing();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    // This function will be called when the audio device is started, or when
+    // its settings (i.e. sample rate, block size, etc) are changed.
+
+    // You can use this function to initialise any resources you might need,
+    // but be careful - it will be called on the audio thread, not the GUI thread.
+
+    // For more details, see the help for AudioProcessor::prepareToPlay()
+}
+
+void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+{
+    // Your audio-processing code goes here!
+
+    // For more details, see the help for AudioProcessor::getNextAudioBlock()
+
+    // Right now we are not producing any data, in which case we need to clear the buffer
+    // (to prevent the output of random noise)
+    bufferToFill.clearActiveBufferRegion();
+}
+
+void MainComponent::releaseResources()
+{
+    // This will be called when the audio device stops, or when it is being
+    // restarted due to a setting change.
+
+    // For more details, see the help for AudioProcessor::releaseResources()
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+//	g.setColour(Colours::red);
+//
+//	for (int i = 0; i < blobState.size(); i++)
+//	{
+//		g.fillEllipse(curState[i][0], curState[i][1], curState[i][2], curState[i][3]);
+//	}
+
+}
+
+void MainComponent::createNextState()
+{
+	auto padding = maxBallRadius;
+	auto height = .5 - padding;
+	auto width = .5 - padding;
+	auto midpoint = width / 2 + padding / 2;
+	auto difference = width - midpoint - padding / 2;
+	auto circleMaxDim = .8;
+	auto circleMinDim = .05;
+
+	for (int i = 0; i < blobState.size(); i+=2)
+	{
+		auto x = r.nextFloat() * difference;
+		auto y = r.nextFloat() * height + padding / 2;
+		auto dim = (r.nextFloat() * (circleMaxDim - circleMinDim)) + circleMinDim;
+
+		auto& left = blobState[i].second;
+		auto& right = blobState[(int64)i + (int64)1].second;
+
+		left[0] = midpoint + x - dim / 2;
+		left[1] = y - dim / 2;
+		left[2] = dim;
+		left[3] = dim;
+
+		right[0] = midpoint - x - dim / 2;
+		right[1] = y - dim / 2;
+		right[2] = dim;
+		right[3] = dim;
+	}	
+}
+
+void MainComponent::copyNextToCurState()
+{
+	for (int i = 0; i < blobState.size(); ++i) 
+	{
+		auto& oldState = blobState[i].first;
+		auto& newState = blobState[i].second;
+
+		for (int j = 0; j < oldState.size(); j++)
+		{
+			oldState[j] = newState[j];
+		}
+	}
+}
+
+
+void MainComponent::translate(float progress)
+{
+	for (int i = 0; i < blobState.size(); i++)
+	{
+		auto from = blobState[i].first;
+		auto to = blobState[i].second;
+
+		// Create a new distance between points
+		//auto distance = sqrt(pow((to[0] - from[0]), 2.0) + pow((to[1] - from[1]), 2.0));
+		auto boxSize = to[2] - from[2];
+
+
+		curState[i][0] = from[0] +  progress * (float)((double)to[0] - from[0]);
+		
+		curState[i][1] = from[1] +  progress * (float)((double)to[1] - from[1]);
+		curState[i][2] = curState[i][3] = progress * boxSize + from[2];
+	}
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
+
+void MainComponent::newOpenGLContextCreated() {}
+void MainComponent::openGLContextClosing()
+{
+    openGLContext.detach();
+    openGLContext.deactivateCurrentContext();
+};
+
+
+void MainComponent::renderOpenGL()
+{
+    OpenGLHelpers::clear (Colours::black);
+
+    //glEnable (GL_BLEND);
+    //glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    double scaleFactor; // class member
+    scaleFactor = juce::Desktop::getInstance().getDisplays().getMainDisplay().scale; //in class' constructor
+    if(scaleFactor > 1) { //setTransform(juce::AffineTransform::scale(scaleFactor, scaleFactor));
+        
+    } //after the Openglcontext was initialized
+
+    for (int i = 0; i < 1; i++)
+    {
+//        glBegin(GL_TRIANGLE_FAN);
+//           glColor3f(0.0f, 0.0f, 1.0f);  // Blue
+//           glVertex2f(0.0f, 0.0f);       // Center of circle
+//           int numSegments = 100;
+//           GLfloat angle;
+//           for (int i = 0; i <= numSegments; i++) { // Last vertex same as first vertex
+//              angle = i * 2.0f * PI / numSegments;  // 360 deg for all segments
+////          glVertex2f(cos(angle) * curState[i][2], sin(angle) * curState[i][2]);
+//              glVertex2f(cos(angle) * .5, sin(angle) * .5);
+//           }
+//        glEnd();
+    }
+}
---
+++ LegacyOpenGL/Source/MainComponent.h
@@ -0,0 +1,60 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent   : public AudioAppComponent , public Timer, public OpenGLRenderer
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent();
+
+    //==============================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    //==============================================================================
+    void paint (Graphics& g) override;
+    void resized() override;
+
+	void createNextState();
+	void copyNextToCurState();
+	void translate(float elapsed);
+	void timerCallback() override;
+    
+    
+    //==============================================================================
+    void newOpenGLContextCreated() override;
+    void openGLContextClosing() override;
+    void renderOpenGL() override;
+private:
+    //==============================================================================
+    OpenGLContext openGLContext;
+    GLdouble clipAreaXLeft, clipAreaXRight, clipAreaYBottom, clipAreaYTop;
+    GLfloat ballXMax, ballXMin, ballYMax, ballYMin;
+    
+	Random r;
+	int blobCount = 150; // Has to be divisible by 2
+	std::vector<std::pair<std::vector<float>, std::vector<float>>> blobState; // now, before<x loc, y loc, size, size> 
+	std::vector<std::vector<float>> curState; // x offset, x loc, y loc, size, size
+	int translateTimeHz = 180; // hz it takes for blobs to travel from one state to next
+	int updatePerSecond = 1;
+	int pauseTimeHz = 2 * 60;
+	int transitionCounter = 0;
+    float maxBallRadius = .2;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
